5c0c17c0835d0d1b39fb891ec3d6f330e05d1b3a
Pochenlin513 <pochenlin513@gmail.com>
2020-04-01 13:40:58 +0800

    Demo ver.

--- 4_5_LCD_QC1602A_demo/main.cpp
+++ 4_5_LCD_QC1602A_demo/main.cpp
@@ -5,15 +5,14 @@ TextLCD lcd(D2, D3, D4, D5, D6, D7);
 
 int main()
 {
-      lcd.printf("Hello World!\n");
-      wait(0.5);
-      lcd.printf("\n  Starting Demo...");
-      wait(0.5);
-      lcd.printf("\n106061227\n");
-      wait(0.5);
-      for(int x = 30; x >= 0; x--){
-          lcd.locate(5,1);
-          lcd.printf("%2D", x);
-          wait(0.5);
-      }
+    lcd.printf("Starting Demo...\n");
+    wait(1);
+    lcd.cls();
+    lcd.printf("106061227\n");
+    wait(0.5);
+    for(int x = 30; x >= 0; x--){
+        lcd.locate(5,1);
+        lcd.printf("%2D", x);
+        wait(0.5);
+    }
 }
